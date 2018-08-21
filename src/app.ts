25aa7baedd6692cebb8a5af20a8a53113c67a752
yojji-3 <lugovskaya.tatya@mail.ru>
2018-08-21 17:45:43 +0300

    feature/add-connections: connect to db, redis

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,25 @@ import * as bodyParser from 'koa-bodyparser';
 import * as path from 'path';
 import * as swagger from 'swagger-injector';
 import { config } from './config';
+import exceptionFilter from './core/middleware/exception-filter.middleware';
+import dbConnection from './db-connection';
 import { configureAppRouter } from './routing';
+import { createClient } from './redis-connection';
 
 const port = config.get('app:port');
-
 const app = new Koa();
-
 app.use(bodyParser());
+app.use(exceptionFilter);
 app.use(
   swagger.koa({
     path: path.join(__dirname, '../swagger.json'),
-  },
-));
+  }),
+);
 app.use(configureAppRouter());
 
-app.listen(port);
+
+dbConnection()
+  .authenticate()
+  .then(() => createClient())
+  .then(() => app.listen(port, () => console.log(`Listening on port ${port}`)))
+  .catch(console.error);
