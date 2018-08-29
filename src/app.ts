cbfaf3864888b42de125126760ecb0a3e69a6538
yojji-3 <lugovskaya.tatya@mail.ru>
2018-08-29 17:33:26 +0300

    feature/project-setup: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import * as Koa from 'koa';
-import * as cors from 'koa2-cors';
 import * as bodyParser from 'koa-bodyparser';
+import * as cors from 'koa2-cors';
 import * as path from 'path';
 import * as swagger from 'swagger-injector';
 import { config } from './config';
 import exceptionFilter from './core/middleware/exception-filter.middleware';
 import dbConnection from './db-connection';
-import { configureAppRouter } from './routing';
 import { createClient } from './redis-connection';
+import { configureAppRouter } from './routing';
 
 const port = config.get('app:port');
 const app = new Koa();
@@ -21,9 +21,8 @@ app.use(
 );
 app.use(configureAppRouter());
 
-
 dbConnection()
   .authenticate()
   .then(() => createClient())
   .then(() => app.listen(port, () => console.log(`Listening on port ${port}`)))
-  .catch(console.error);
+  .catch(console.log);
