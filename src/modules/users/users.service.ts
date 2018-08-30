69636d71248bf18514acd71aa363c969d619cf51
yojji-3 <lugovskaya.tatya@mail.ru>
2018-08-30 12:57:29 +0300

    fix/services: refactoring users and auth services

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,11 +1,16 @@
+import { USER_NOT_FOUND } from '../../core/constants/error.constants';
+import NotFoundException from '../../core/exceptions/not-found.exception';
 import User from '../../models/User';
 
-const findUserByEmail = (email: string) => {
-  return User.findOne({
+const findUserByEmail = async (email: string) => {
+  const user = await User.findOne({
     where: { email },
-  }).then(user =>
-    user ? user.get() : null
-  );
+  });
+  if (user) {
+    return user;
+  } else {
+    throw new NotFoundException(USER_NOT_FOUND);
+  }
 };
 
 export default {
