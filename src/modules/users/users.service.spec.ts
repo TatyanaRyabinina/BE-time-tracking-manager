b354294e6b9a2d249f2b24d9b5938d6255de43f1
yojji-3 <lugovskaya.tatya@mail.ru>
2018-08-31 13:52:07 +0300

    feature/unit-test: add unit tests for users.service

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,5 +1,21 @@
+import NotFoundException from '../../core/exceptions/not-found.exception';
+import { TEST_USER_EMAIL, WRONG_TEST_USER_EMAIL } from '../../models/__mocks__/User';
+import UserService from './users.service';
+
+jest.mock('../../models/User');
+
 describe('users.service test', () => {
-  it('Fake test', () => {
-    expect(true).toBeTruthy();
+  it('findUserByEmail method success', async () => {
+    const user = await UserService.findUserByEmail(TEST_USER_EMAIL);
+    expect(user.email).toEqual(TEST_USER_EMAIL);
+  });
+
+  it('findUserByEmail throw NotFoundException', async () => {
+    try {
+      await UserService.findUserByEmail(WRONG_TEST_USER_EMAIL);
+      expect(false).toBeTruthy();
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotFoundException);
+    }
   });
 });
