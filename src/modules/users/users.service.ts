90568d80c124b3b48b065941612b167a8bac8cca
yojji-3 <lugovskaya.tatya@mail.ru>
2018-09-03 14:47:30 +0300

    feature/registration: add user registration

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,34 @@
-import { USER_NOT_FOUND } from '../../core/constants/error.constants';
+import { USER_NOT_FOUND, USER_IS_EXIST } from '../../core/constants/error.constants';
+import BadRequestException from '../../core/exceptions/bad-request.exception';
 import NotFoundException from '../../core/exceptions/not-found.exception';
+
 import User from '../../models/User';
+
+interface IUserField {
+  email: string;
+  firstName: string;
+  lastName: string;
+}
+
+const createUser = async (userData: IUserField) => {
+  try {
+    await findUserByEmail(userData.email);
+    throw new BadRequestException(USER_IS_EXIST);
+  } catch (err) {
+    if (err instanceof NotFoundException) {
+      return await addNewUser(userData);
+    }
+    if (err instanceof BadRequestException) {
+      throw err;
+    }
+  }
+};
+
+const addNewUser = (user: IUserField) => {
+  return User.create(
+    user,
+  );
+};
 
 const findUserByEmail = async (email: string) => {
   const user = await User.findOne({
@@ -15,4 +43,5 @@ const findUserByEmail = async (email: string) => {
 
 export default {
   findUserByEmail,
+  createUser,
 };
