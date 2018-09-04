74a2cfd8208366084fd683da02f4a10f06e9310b
yojji-3 <lugovskaya.tatya@mail.ru>
2018-09-04 18:15:02 +0300

    feature/unit-test: add tests for users.service methods

--- src/models/__mocks__/User.ts
+++ src/models/__mocks__/User.ts
@@ -1,14 +1,38 @@
+import { UpdateUserDto } from '../../modules/users/dto/update-user.dto';
 import { BasicMock } from './BaseMock';
 
 export const TEST_USER_EMAIL = 'qwerty@qwe.rty';
 export const WRONG_TEST_USER_EMAIL = 'wrongemail@gwe.rty';
 
-const user = {
+export const creatingUser = {
+  email: 'create@user.com',
+  firstName: 'TestName',
+  lastName: 'LastName',
+};
+
+export const existingUser = {
   email: TEST_USER_EMAIL,
   firstName: 'Ivan',
   lastName: 'Ivanov',
-  birthdayDate: '1992-12-01',
-  hireDate: '2017-12-11',
 };
 
-export default new BasicMock(user);
+this.existingUser.update = (data: UpdateUserDto) => {
+  return data;
+};
+
+export const profileData = {
+  firstName: 'Ivan',
+  lastName: 'Ivanov',
+  avatar: 'avatarstring',
+  birthdayDate: new Date(1992, 12, 1),
+  hireDate: new Date(2017, 12, 11),
+  phoneNumber: '0501231230',
+  telegram: '@telegramname',
+  skype: 'skypename',
+  slack: 'slackname@qwe.er',
+  departmentId: 0,
+  professionId: 0,
+  professionalLevelId: 0,
+};
+
+export default new BasicMock(existingUser);
