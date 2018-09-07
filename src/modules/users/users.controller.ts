1d929f8e1f27a769402d9dbec05b28a384adc9ad
yojji-3 <lugovskaya.tatya@mail.ru>
2018-09-07 14:56:11 +0300

    feature/user-module:  update findUserByEmail

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,22 +3,12 @@ import { Context } from 'koa';
 import * as Router from 'koa-router';
 import { validateBody } from '../../core/middleware/validate.middleware';
 import { IUserJwt } from '../auth/interfaces';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import UserService from './users.service';
 
 const getUser = async (ctx: Context) => {
   const userJwt = ctx.request.body as IUserJwt;
   const data = await UserService.findUserBy({ id: userJwt.id });
-  ctx.body = {
-    data,
-  };
-};
-
-const registerUser = async (ctx: Context) => {
-  const { email, firstName, lastName } = ctx.request.body as CreateUserDto;
-  const data = await UserService.createUser({ email, firstName, lastName });
-  ctx.status = HttpStatus.OK;
   ctx.body = {
     data,
   };
@@ -35,7 +25,6 @@ const updateUser = async (ctx: Context) => {
 
 export default () => {
   const userController: Router = new Router();
-  userController.post('/registration', validateBody(CreateUserDto), registerUser);
   userController.get('/user', getUser);
   userController.put('/user', validateBody(UpdateUserDto), updateUser);
   return userController.routes();
