7c6f7eafba1cd1be582148f241a9f61879c60a3f
Ildar <ildarka008@gmail.com>
2018-09-07 17:48:23 +0300

    fix/validation: update ability to get query or params from ctx

--- src/core/middleware/validate.middleware.ts
+++ src/core/middleware/validate.middleware.ts
@@ -2,11 +2,12 @@ import { plainToClass } from 'class-transformer';
 import { ClassType } from 'class-transformer/ClassTransformer';
 import { validate } from 'class-validator';
 import { Context } from 'koa';
+import { get } from 'lodash';
 import { ValidationException } from '../exceptions/validation.exception';
 
 const validateMiddleware = (ToValidateClass: ClassType<{}>, target: string) => {
   return async (ctx: Context, next: () => Promise<void>) => {
-    const toValidateInstance = plainToClass(ToValidateClass, ctx.request[target]);
+    const toValidateInstance = plainToClass(ToValidateClass, get(ctx, target), {});
     const errors = await validate(toValidateInstance, {
       validationError: { target: false },
     });
