b54f78ae2eed3b190bb49a47ba7459213c0fda25
yojji-3 <lugovskaya.tatya@mail.ru>
2018-09-12 18:37:37 +0300

    feature/routing: update configuration

--- src/routing.ts
+++ src/routing.ts
@@ -2,7 +2,10 @@ import * as jwt from 'koa-jwt';
 import * as Router from 'koa-router';
 import { config } from './config';
 import configureAuthController from './modules/auth/auth.controller';
+import configureCustomerController from './modules/customers/customers.controller';
 import configureTimeTrackController from './modules/time-track/time-track.controller';
+import { setIsAdmin } from './modules/users/middleware/acl.middleware';
+import configureRegisterController from './modules/users/register.controller';
 import configureUserController from './modules/users/users.controller';
 
 const jwtMiddleware = jwt({ secret: config.get('jwt:secret') });
@@ -10,13 +13,16 @@ const jwtMiddleware = jwt({ secret: config.get('jwt:secret') });
 const configureGuest = () => {
   const guestRouter = new Router();
   guestRouter.use(configureAuthController());
-  guestRouter.use(configureUserController());
+  guestRouter.use(configureRegisterController());
   return guestRouter.routes();
 };
 
 const configurePrivate = () => {
   const privateRouter = new Router();
   privateRouter.use(jwtMiddleware);
+  privateRouter.use(setIsAdmin);
+  privateRouter.use(configureUserController());
+  privateRouter.use(configureCustomerController());
   privateRouter.use(configureTimeTrackController());
   return privateRouter.routes();
 };
